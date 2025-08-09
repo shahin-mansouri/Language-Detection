0d1cfff07079e153e7c41f27a4bf6149f958e4aa
Shahin <mansouri.math.kntu@gmail.com>
2025-08-09 14:37:57 +0330

    Add new apps and update URL routing for accounts, analytics, api, and detection

--- language_detection/settings.py
+++ language_detection/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
+    'analytics',
+    'api',
+    'detection',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
